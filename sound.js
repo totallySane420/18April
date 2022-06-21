f7bdae86c2485d1412c954201bf4b075c3676a83
Vikas Maury <62638545+vikasz1@users.noreply.github.com>
2022-06-21 21:32:39 +0530

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,28 +1,18 @@
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
 
-window.addEventListener("keydown",(e)=>{
-    // console.log(e)
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
-    console.log(audio)
-});
-function removeTransition(e){
-    if (e.propertyname!='transform') return;
-    e.target.classList.remove('playing')
-}
-function playSound(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
-    if (!audio) {return};
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
 
-    key.classList.add('playing')
+    key.classList.add('playing');
     audio.currentTime = 0;
-    audio.play()
-}
+    audio.play();
+  }
 
-const keys = Array.from(document.querySelectorAll(".key"))
-keys.forEach(
-    (key)=>{
-        key.addEventListener('transitioned', removeTransition)
-    }  
-)
-window.addEventListener('keydown', playSound);
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
